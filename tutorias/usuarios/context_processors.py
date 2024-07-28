47e3c0c8eb0953b6e238ec505fda626d56c5e8fd
ivansoza <ivansz123@gmail.com>
2024-07-28 17:45:12 -0600

    e2wedq

--- tutorias/usuarios/context_processors.py
+++ tutorias/usuarios/context_processors.py
@@ -3,7 +3,11 @@
 def user_info(request):
     user_info_dict = {}
     if request.user.is_authenticated:
+        full_name = ' '.join(filter(None, [request.user.first_name, request.user.last_name]))
+
         user_info_dict['user_name'] = request.user.username
+        user_info_dict['full_name'] = full_name if full_name else 'Sin nombre especificado'
+
         user_info_dict['user_gender'] = request.user.gender if hasattr(request.user, 'gender') else None
     return {'user_info': user_info_dict}
 
